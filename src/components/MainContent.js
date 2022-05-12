c69a907a99d181b2bac6fae59ff6b27717b7e7f9
UCAkkaya <uc.akkaya@gmail.com>
2022-05-12 20:03:01 +0200

    new colors and components

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,15 +1,15 @@
 import React from "react"
+import About from "./About"
+import Team from "./Team.js"
+
 export default function MainContent() {
     return (  
     <div className="main-content">
-        <h1>Osiris</h1>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit ut aliquam purus sit. In eu mi bibendum neque egestas congue. Sed euismod nisi porta lorem. Nunc id cursus metus aliquam eleifend mi in nulla. Viverra adipiscing at in tellus integer feugiat. Libero justo laoreet sit amet cursus sit amet dictum. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Pellentesque dignissim enim sit amet venenatis. Rhoncus mattis rhoncus urna neque viverra justo. Dictum non consectetur a erat nam at lectus urna duis. Integer malesuada nunc vel risus. Sed nisi lacus sed viverra tellus in hac habitasse platea. Massa sapien faucibus et molestie ac feugiat sed lectus vestibulum. Eget nunc scelerisque viverra mauris in. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. At augue eget arcu dictum varius duis at consectetur. Pretium fusce id velit ut tortor pretium. Pellentesque massa placerat duis ultricies.
-        </p>
-
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit ut aliquam purus sit. In eu mi bibendum neque egestas congue. Sed euismod nisi porta lorem. Nunc id cursus metus aliquam eleifend mi in nulla. Viverra adipiscing at in tellus integer feugiat. Libero justo laoreet sit amet cursus sit amet dictum. Tincidunt vitae semper quis lectus nulla at volutpat diam ut. Pellentesque dignissim enim sit amet venenatis. Rhoncus mattis rhoncus urna neque viverra justo. Dictum non consectetur a erat nam at lectus urna duis. Integer malesuada nunc vel risus. Sed nisi lacus sed viverra tellus in hac habitasse platea. Massa sapien faucibus et molestie ac feugiat sed lectus vestibulum. Eget nunc scelerisque viverra mauris in. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. At augue eget arcu dictum varius duis at consectetur. Pretium fusce id velit ut tortor pretium. Pellentesque massa placerat duis ultricies.
-
-        </p>
+        <div className="about-content">
+        <About />
+        </div>
+        <div className="team-content"> 
+        <Team />
+        </div>
     </div>)
 }
