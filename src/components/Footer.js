92899b168bd7d9f241305768cc6c9c9c0ab2c294
UCAkkaya <uc.akkaya@gmail.com>
2022-05-19 10:41:20 +0200

    Updates

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Footer() {
-    return(
-    <footer>
-        <small className="footer">© 2022 UCA development. All rights reserved</small>
-    </footer>
-    )
-}
